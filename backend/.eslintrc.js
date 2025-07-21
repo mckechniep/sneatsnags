ba7cd834813740c94b150489355473d46e521ffb
samuel922 <onyangosamuel583@gmail.com>
2025-07-21 21:56:29 +0300

    updated code

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
     sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
   ],
@@ -13,15 +14,63 @@ module.exports = {
     es6: true,
   },
   ignorePatterns: [
-    'dist',
-    'node_modules',
+    'dist/**',
+    'node_modules/**',
     '*.js',
     '*.d.ts',
-    'prisma',
+    'prisma/migrations/**',
+    'create-admin.js',
+    'create-admin-only.js',
   ],
   rules: {
-    'no-unused-vars': 'off', // Turn off for TypeScript files
-    'no-console': 'off',
+    // TypeScript specific rules - Make them warnings for build to pass
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_|^next$|^req$|^res$|^error$',
+        varsIgnorePattern: '^_|^[A-Z]',
+        ignoreRestSiblings: true,
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    
+    // General rules
+    'no-unused-vars': 'off', // Use TypeScript version instead
+    'no-console': 'off', // Allow console.log in backend
     'no-undef': 'off', // TypeScript handles this
+    'prefer-const': 'warn', // Make this a warning instead of error
+    'no-var': 'error',
   },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.spec.ts'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['src/prisma/seed.ts'],
+      rules: {
+        'no-console': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['src/controllers/**/*.ts', 'src/middlewares/**/*.ts'],
+      rules: {
+        // Allow unused imports and variables that are common in Express controllers
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_|^next$|^req$|^res$',
+            varsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+      },
+    },
+  ],
 };
